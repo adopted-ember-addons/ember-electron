5d81d7d90fc7dc2307b7f7c5e97f05763adc8395
Felix Rieseberg <felix@felixrieseberg.com>
2016-08-02 11:07:09 -0700

    :wrench: Add semicolons back

--- config/environment.js
+++ config/environment.js
@@ -26,27 +26,27 @@ module.exports = function (environment) {
   }
 
   if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true
-    // ENV.APP.LOG_ACTIVE_GENERATION = true
-    // ENV.APP.LOG_TRANSITIONS = true
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true
-    // ENV.APP.LOG_VIEW_LOOKUPS = true
+    // ENV.APP.LOG_RESOLVER = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_TRANSITIONS = true;
+    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/'
-    ENV.locationType = 'none'
+    ENV.baseURL = '/';
+    ENV.locationType = 'none';
 
     // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false
-    ENV.APP.LOG_VIEW_LOOKUPS = false
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
 
-    ENV.APP.rootElement = '#ember-testing'
+    ENV.APP.rootElement = '#ember-testing';
   }
 
   if (environment === 'production') {
   }
 
-  return ENV
+  return ENV;
 }
