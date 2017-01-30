55bb5ce152c37a62b50650f9150b4ee3aeb02570
@anulman <anulman@gmail.com>
2017-01-30 18:07:35 -0500

    feat(forge-blueprint): update blueprint to es6 class syntax + use forge import api

--- blueprints/ember-electron/index.js
+++ blueprints/ember-electron/index.js
@@ -1,144 +1,93 @@
-const fs = require('fs')
-const path = require('path')
-const chalk = require('chalk')
-const RSVP = require('rsvp')
+const RSVP           = require('rsvp');
 const VersionChecker = require('ember-cli-version-checker')
+const fs             = require('fs-extra');
+const path           = require('path');
 
-const denodeify = RSVP.denodeify
-const readFile = denodeify(fs.readFile)
-const writeFile = denodeify(fs.writeFile)
+const Blueprint      = require('ember-cli/lib/models/blueprint');
+const Logger         = require('ember-electron/lib/utils/logger');
+const forgeImport    = require('electron-forge/dist/api/import').default;
 
-module.exports = {
-  description: 'Install ember-electron in the project.',
+const {
+  denodeify,
+  resolve
+} = RSVP;
 
-  normalizeEntityName: function (entityName) {
-    return entityName
-  },
+const {
+  readJson,
+  writeJson
+} = fs;
 
-  afterInstall: function (options) {
-    const dependencies = this.project.dependencies()
+class EmberElectronBlueprint extends Blueprint {
+  constructor(options) {
+    super(options);
 
-    return this.addElectronConfig(options).then(() => {
-      this.logConfigurationWarning()
+    this.description = 'Install ember-electron in the project.';
+  }
 
-      const packages = []
-      if (!dependencies['electron'] && !dependencies['electron-prebuilt']) {
-        packages.push({name: 'electron'})
-      }
+  normalizeEntityName(entityName) {
+    return entityName;
+  }
 
-      if (!dependencies['electron-packager']) {
-        packages.push({name: 'electron-packager'})
-      }
+  afterInstall(options) {
+    let logger = new Logger(this);
 
-      if (!dependencies['electron-rebuild']) {
-        packages.push({name: 'electron-rebuild'})
-      }
+    return forgeImport()
+      .then(() => this._ensurePackageJsonMainConfigured())
+      .then(() => this._ensureElectronPrebuiltCompile())
+      .then(() => {
+        // todo: update readme once upstream forge needs confirmed
+        let configMessage = 'Ember Electron requires configuration. Please consult the Readme to ensure that this addon works!'
 
-      if (!dependencies['ember-inspector']) {
-        packages.push({name: 'ember-inspector'})
-      }
+        logger.message(configMessage, logger.chalk.yellow);
+        logger.message('https://github.com/felixrieseberg/ember-electron');
+      });
+  }
 
-      if (!dependencies.devtron) {
-        packages.push({name: 'devtron'})
-      }
+  // todo: replace with direct replacement in config/env & log warnings
+  locals(options) {
+    const checker = new VersionChecker(this);
+    const dep = checker.for('ember-cli', 'npm');
 
-      if (packages.length > 0) {
-        return this.addPackagesToProject(packages)
-      }
-    })
-  },
-
-  addElectronConfig: function (options) {
-    const packageJsonPath = path.join(this.project.root, 'package.json')
-
-    if (this.project.pkg.main) {
-      return RSVP.resolve()
-    }
-
-    const prom = readFile(packageJsonPath, {
-      encoding: 'utf8'
-    })
-
-    return prom.then((data) => {
-      const json = JSON.parse(data)
-
-      json.main = 'electron.js'
-      json['ember-electron'] = {
-        'WHAT IS THIS?': 'Please see the README.md',
-        'copy-files': ['electron.js', 'package.json'],
-        'name': null,
-        'platform': null,
-        'arch': null,
-        'version': null,
-        'app-bundle-id': null,
-        'app-category-type': null,
-        'app-copyright': null,
-        'app-version': null,
-        'asar': null,
-        'asar-unpack': null,
-        'asar-unpack-dir': null,
-        'build-version': null,
-        'cache': null,
-        'extend-info': null,
-        'extra-resource': null,
-        'helper-bundle-id': null,
-        'icon': null,
-        'ignore': null,
-        'out': null,
-        'osx-sign': {
-          identity: null,
-          entitlements: null,
-          'entitlements-inherit': null
-        },
-        'protocol': [],
-        'protocol-names': [],
-        'win-opts': {
-          'loading-gif': null,
-          'icon-url': null,
-          'remote-releases': null,
-          'certificate-file': null,
-          'certificate-password': null,
-          'sign-with-params': null
-        },
-        overwrite: null,
-        prune: null,
-        'strict-ssl': null,
-        'win32metadata': {
-          CompanyName: null,
-          FileDescription: null,
-          OriginalFilename: null,
-          ProductName: null,
-          InternalName: null
-        }
-      }
-
-      this.ui.writeLine('  ' + chalk.yellow('overwrite') + ' package.json')
-
-      if (!options.dryRun) {
-        return writeFile(packageJsonPath, JSON.stringify(json, null, '  '))
-      }
-    })
-  },
-
-  logConfigurationWarning: function () {
-    const info = 'Ember Electron requires configuration. Please consult the Readme to ensure that this addon works!'
-    const url = 'https://github.com/felixrieseberg/ember-electron'
-
-    this.ui.writeLine(chalk.yellow(info))
-    this.ui.writeLine(chalk.green(url))
-  },
-
-  locals: function () {
-    const checker = new VersionChecker(this)
-    const dep = checker.for('ember-cli', 'npm')
-    let baseURLOption = "baseURL: '/',"
-    let baseURLTestOption = "ENV.baseURL = '/';"
+    let baseURLOption = "baseURL: '/',";
+    let baseURLTestOption = "ENV.baseURL = '/';";
 
     if (dep.satisfies('>= 2.7.0')) {
-      baseURLOption = 'rootURL: null,'
-      baseURLTestOption = ''
+      baseURLOption = 'rootURL: null,';
+      baseURLTestOption = '';
     }
 
-    return {baseURLOption, baseURLTestOption}
+    return { baseURLOption, baseURLTestOption };
   }
-}
+
+  _ensurePackageJsonMainConfigured() {
+    let packageJsonPath = path.join(this.project.root, 'package.json');
+
+    if (this.project.pkg.main !== undefined) {
+      return resolve();
+    }
+
+    // todo: n.b. felix rm'ed dry run
+    return denodeify(readJson)(packageJsonPath)
+      .then((json) => {
+        json.main = 'electron.js';
+        return denodeify(writeJson)(packageJsonPath, json);
+      });
+  }
+
+  // todo: rm once defaults are supported by forge import
+  //       - else will need to override addPackageToProject and use --save-exact
+  //       https://github.com/electron-userland/electron-forge/issues/108#issuecomment-275970938
+  _ensureElectronPrebuiltCompile() {
+    let dependencies = this.project.dependencies();
+    let dep = {
+      name: 'electron-prebuilt-compile',
+      target: '1.4.15'
+    };
+
+    if (dependencies[dep.name] === undefined) {
+      return this.addPackageToProject(dep.name, dep.target);
+    }
+  }
+};
+
+module.exports = EmberElectronBlueprint;
