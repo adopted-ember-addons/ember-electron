43c087b55c10cd3e2167b14d29667854b2c54e45
@anulman <anulman@gmail.com>
2017-03-16 01:59:13 -0400

    style(require): rm long comments & otherwise tidy

--- app/electron/adjust-module-search-paths.js
+++ app/electron/adjust-module-search-paths.js
@@ -1,19 +1,7 @@
-// This function needs to be ES5 compatible since it may otherwise cause `uglify` to choke
 (function() {
-  // Exit immediately if we're not running in Electron
   if (!window.ELECTRON) {
     return;
   }
-
-  // Electron restricts the node_modules search path such that only modules within
-  // its resources directory are allowed.
-  // https://github.com/electron/electron/blob/master/lib/common/reset-search-paths.js
-  //
-  // Unfortunately, during development a pre-built electron setup might be used and
-  // have its resources folder in some other folder than the app being served.
-  // To work around this we can add these paths back into the global list.
-  //
-  // Note: This should probably be handled upstream in electron-prebuilt-compiler
 
   var fs = window.requireNode('fs');
   var path = window.requireNode('path');
@@ -24,30 +12,28 @@
     path.resolve(window.processNode.resourcesPath, 'app', 'node_modules')
   ];
 
-  // Check if we're running with resource root inside of a `electron-prebuilt-compile` dir.
-  // This should correspond to when running `ember electron`
+  // n.b. if we are running in `electron-prebuilt-compile`,
+  // TODO @jacobq is this still necessary given window.processNoce.cwd() above?
+  //      - if so, plz explain what __dirname is, why you're slicing, etc
   var epcIndex = __dirname.indexOf('electron-prebuilt-compile');
-  if (epcIndex !== -1)
+  if (epcIndex !== -1) {
     pathsToAdd.push(__dirname.slice(0, epcIndex));
-
-  // When running a packaged app, we have our modules in
-  // resources/app/node_modules
-  pathsToAdd.push(path.resolve(window.processNode.resourcesPath, 'app', 'node_modules'));
+  }
 
   pathsToAdd.forEach(function(pathToAdd) {
-    // Note: sync version is used on purpose here since otherwise later calls to `require`
-    // may fail because they need to use these paths but this hasn't finished yet.
     try {
+      // n.b. use statSync to prevent race conditions
       var stats = fs.statSync(pathToAdd);
-      if (stats.isDirectory() && module.Module.globalPaths.indexOf(pathToAdd) === -1) {
-        module.Module.globalPaths.push(pathToAdd);
+
+      // TODO @jacobq explain why you are setting globalPaths vs paths
+      if (stats.isDirectory() && module.globalPaths.indexOf(pathToAdd) === -1) {
+        module.globalPaths.push(pathToAdd);
       }
-    }
-    catch(e) {
-      // It's OK if any of these paths don't exist; we just won't add them.
-      // But if there's some other error we probably still want to know about it
-      if (e.code !== 'ENOENT')
+    } catch(e) {
+      // n.b. ignore "dir does not exist" errors, else throw
+      if (e.code !== 'ENOENT') {
         throw e;
+      }
     }
   });
 }());
