a558251a3544a9ace140b54f9205b7f6e8dc3b86
Florian Pichler <f.pichler@anfe.ma>
2017-04-18 13:38:55 +0200

    Update Ember-CLI to latest release
    This should make the problems in #231 go away as 2.12 properly handles installs with yarn. This also includes a bunch of blueprint cleanup to get is more in line with the base ember-cli addon blueprint.

--- config/environment.js
+++ config/environment.js
@@ -1,46 +1,6 @@
-/* jshint node: true */
+/* eslint-env node */
+'use strict';
 
-module.exports = function(environment) {
-  let ENV = {
-    modulePrefix: 'ember-electron',
-    environment,
-    rootURL: '/',
-    locationType: 'hash',
-    EmberENV: {
-      FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
-      },
-    },
-
-    APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
-    },
-  };
-
-  if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
-  }
-
-  if (environment === 'test') {
-    // Testem prefers this...
-    ENV.locationType = 'none';
-
-    // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
-
-    ENV.APP.rootElement = '#ember-testing';
-  }
-
-  if (environment === 'production') {
-    //
-  }
-
-  return ENV;
+module.exports = function(/* environment, appConfig */) {
+  return { };
 };
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,13 +1,13 @@
-/* global require, module */
-let EmberApp = require('ember-cli/lib/broccoli/ember-addon');
+/* eslint-env node */
+const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  let app = new EmberApp(defaults, {
+  var app = new EmberAddon(defaults, {
     // Add options here
   });
 
   /*
-    This build file specifes the options for the dummy test app of this
+    This build file specifies the options for the dummy test app of this
     addon, located in `/tests/dummy`
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
