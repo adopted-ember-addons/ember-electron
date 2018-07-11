6b2601486f3f125926fc485b33b88b719c08d6e6
Jacob Quant <jacobq@gmail.com>
2018-07-11 15:28:44 -0500

    ember-cli-update --to 2.18

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,12 @@ module.exports = {
     ecmaVersion: 2017,
     sourceType: 'module',
   },
+  plugins: [
+    'ember',
+  ],
   extends: [
     'eslint:recommended',
-    'plugin:ember-suave/recommended',
+    'plugin:ember/recommended',
   ],
   env: {
     es6: true,
@@ -46,4 +49,42 @@ module.exports = {
     'no-console': 'off',
     'one-var': 'off',
   },
+  overrides: [
+    // node files
+    {
+      files: [
+        'index.js',
+        'testem.js',
+        'ember-cli-build.js',
+        'config/**/*.js',
+        'tests/dummy/config/**/*.js'
+      ],
+      excludedFiles: [
+        'app/**',
+        'addon/**',
+        'tests/dummy/app/**'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      },
+      plugins: ['node'],
+      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+        // add your custom rules and overrides for node files here
+      })
+    },
+
+    // test files
+    {
+      files: ['tests/**/*.js'],
+      excludedFiles: ['tests/dummy/**/*.js'],
+      env: {
+        embertest: true
+      }
+    }
+  ]
 };
