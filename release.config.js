aec6e53d218a8b02b5fad40670ac5821fe2923d9
Ben Demboski <ben@turbopatent.com>
2019-08-23 17:37:39 -0700

    fix: update semantic release commit message config
    Rather than using eslint's config, this gets us on the default convention-changelog format. At least two maintainers of the project assumed this was the format (resulting in a release not happening because the format was wrong), plus it's aligned with the GitHub app we can install to validate the format for PRs.

--- release.config.js
+++ release.config.js
@@ -1,9 +1,7 @@
 module.exports = {
   branch: 'master',
   plugins: [
-    ['@semantic-release/commit-analyzer', {
-      preset: 'eslint',
-    }],
+    '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
     '@semantic-release/github',
