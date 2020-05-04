5891ef7d222a0bd36a6b9a32c74865997d670d65
Robert Wagner <rwwagner90@gmail.com>
2020-05-04 15:23:26 -0400

    Fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -77,6 +77,7 @@ module.exports = {
         'node/no-missing-require': ['error', {
           'allowModules': [
             'electron',
+            'electron-devtools-installer',
             'ember-electron'
           ],
         }]
