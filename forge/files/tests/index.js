31e50c6779c76062f838b63a74ba0b6760fb6faa
Robert Wagner <rwwagner90@gmail.com>
2020-05-06 13:18:26 -0400

    Remove from deps

--- forge/files/tests/index.js
+++ forge/files/tests/index.js
@@ -1,1 +1,9 @@
+const { app } = require('electron');
+const { default: installExtension, EMBER_INSPECTOR } = require('electron-devtools-installer');
+
+app.on('ready', function onReady() {
+  installExtension(EMBER_INSPECTOR)
+    .catch((err) => console.log('An error occurred: ', err));
+});
+
 require('ember-electron/lib/test-support/test-index');
