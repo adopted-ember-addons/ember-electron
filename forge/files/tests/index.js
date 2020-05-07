90e78eee6b25a22023a65bd01f4ec5422a0bb8cc
Robert Wagner <rwwagner90@gmail.com>
2020-05-06 20:19:57 -0400

    Install devtron from the main process

--- forge/files/tests/index.js
+++ forge/files/tests/index.js
@@ -1,6 +1,7 @@
 const { default: installExtension, EMBER_INSPECTOR } = require('electron-devtools-installer');
 
 require('electron').app.on('ready', function onReady() {
+  require('devtron').install();
   installExtension(EMBER_INSPECTOR)
     .catch((err) => console.log('An error occurred: ', err));
 });
