c80833be94fd9b762572484ea5015459f7229703
Ben Demboski <demboskb@gmail.com>
2020-05-18 09:51:10 -0700

    Simplify test setup
    In ember-electron 2.x, the `electron:test` command would build the Ember app into a temp location somewhere, so we'd have to jump through a bunch of hoops to pass that location to the Electron app so it could load the Ember app into the browser window.
    
    In 3.x, we changed it so that the Ember app is always built into `electron-app/ember-test`, so we don't need to pass this extra information around at runtime. So we're now simplifying and better organizing the test infrastructure:
    
    * Remove the code that handles passing the Ember app location through to the test main process
    * Put the `electron-protocol-serve` setup code into a blueprint-generated utility function. This way the production and test versions of the app use the same code, and it's easier to see the correspondence between the two different usages
    * Reorganize the test support code a bit for better flexibility -- rather than the test `index.js` just requiring a single file that runs all the test main process code, it imports some functions and calls them. This makes it a bit easier to read and understand, makes it easier to share code between the production and test main processes, and also allows more flexibility for users to customize, since the functionality that `ember-electron` supplies isn't all-or-nothing
    * await the Ember Inspector installation before opening the browser window, to make sure we don't have a race condition. This should fix the intermittent failures we've seen in the test that verifies that we install Ember Inspector.
    * Make end-to-end test more robust -- in this test we paste some code into the test `index.js` to verify some functionality, so rather than putting it at module scope, we put it in a function that we call so we don't have to worry about naming conflicts/duplicate imports between the blueprint-generated code and the test code we paste into the same file

--- forge/files/tests/index.js
+++ forge/files/tests/index.js
@@ -1,9 +1,31 @@
 const { default: installExtension, EMBER_INSPECTOR } = require('electron-devtools-installer');
+const path = require('path');
+const { app } = require('electron');
+const setupServeProtocol = require('../src/setup-serve-protocol');
+const { setupTestem, openTestWindow } = require('ember-electron/lib/test-support');
 
-require('electron').app.on('ready', function onReady() {
-  require('devtron').install();
-  installExtension(EMBER_INSPECTOR)
-    .catch((err) => console.log('An error occurred: ', err));
+const emberAppDir = path.resolve(__dirname, '..', 'ember-test');
+
+setupServeProtocol(emberAppDir);
+
+app.on('ready', async function onReady() {
+  try {
+    require('devtron').install();
+  } catch (err) {
+    console.log('Failed to install Devtrom: ', err);
+  }
+  try {
+    await installExtension(EMBER_INSPECTOR);
+  } catch (err) {
+    console.log('Failed to install Ember Inspector: ', err)
+  }
+
+  setupTestem();
+  openTestWindow();
 });
 
-require('ember-electron/lib/test-support/test-index');
+app.on('window-all-closed', function onWindowAllClosed() {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
