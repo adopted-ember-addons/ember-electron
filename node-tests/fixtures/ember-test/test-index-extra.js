2604346b209231fa2668adb16a591e6f5ccaee9b
Ben Demboski <demboskb@gmail.com>
2020-05-19 22:50:14 -0700

    Fix build
    Unfortunately, due to bugs in Electron 9, we can't use devtron (https://github.com/electron/electron/issues/23676). We could at least get Ember Inspector to load (using the workaround for https://github.com/electron/electron/issues/23656), but hopefully Electron will turn around a quick fix at least for that second issue so we don't have to add a temporary workaround to the blueprint-generated code...

--- node-tests/fixtures/ember-test/test-index-extra.js
+++ node-tests/fixtures/ember-test/test-index-extra.js
@@ -8,17 +8,20 @@ function testIndexExtra() {
   const helper = require('../src/helper');
   helper();
 
-  const { app, BrowserWindow } = require('electron');
-
-  app.once('browser-window-created', (event, win) => {
-    win.webContents.on('did-finish-load', () => {
-      let extensions = BrowserWindow.getDevToolsExtensions();
-      if (!Object.prototype.hasOwnProperty.call(extensions, 'devtron') || !Object.prototype.hasOwnProperty.call(extensions, 'Ember Inspector')) {
-        console.error('devtron and/or ember-inspector not installed', Object.keys(extensions)); // eslint-disable-line no-console
-        app.exit(-1);
-      }
-    });
-  });
+  // disabled until there's a fix for https://github.com/electron/electron/issues/23676
+  // and ideally for https://github.com/electron/electron/issues/23656
+  //
+  // const { app, BrowserWindow } = require('electron');
+  //
+  // app.once('browser-window-created', (event, win) => {
+  //   win.webContents.on('did-finish-load', () => {
+  //     let extensions = BrowserWindow.getDevToolsExtensions();
+  //     if (!Object.prototype.hasOwnProperty.call(extensions, 'devtron') || !Object.prototype.hasOwnProperty.call(extensions, 'Ember Inspector')) {
+  //       console.error('devtron and/or ember-inspector not installed', Object.keys(extensions)); // eslint-disable-line no-console
+  //       app.exit(-1);
+  //     }
+  //   });
+  // });
 }
 
 testIndexExtra();
